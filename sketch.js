5c0b98d9cb8aca8ff2e84262c2b4b1d26ed96e3d
mahantvg <64301173+mahantvg@users.noreply.github.com>
2020-05-30 11:08:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var ball;
+
+function setup(){
+    database = firebase.database();
+     console.log(database); 
+     createCanvas(500,500);
+     hypnoticBall = createSprite(250,250,10,10);
+     hypnoticBall.shapeColor = 'red';
+    var hypnoticBallPosition = database.ref('ball/position');
+     hypnoticBallPosition.on("value",readPosition,showError);
+
+
+}
+function readPosition(data){ position = data.val();
+     console.log(position.x);
+      hypnoticBall.x = position.x;
+       hypnoticBall.y = position.y; }
+
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
